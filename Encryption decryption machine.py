a6a21387ac2dc09403b83518a45a5a630ede6bcd
R0und-tech <leonvlad2009@gmail.com>
2025-08-22 22:31:24 +0300

    Update Encryption decryption machine.py
    -Add comments
    -Add encryption func
    -Add word and key word lenght alignment func
    -Still no UI  ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯

--- Encryption decryption machine.py
+++ Encryption decryption machine.py
@@ -1,7 +1,10 @@
 import random
 
 def generate_key_func(length):
-    # Русские буквы в нижнем / верхнем регистре
+    """
+    генерация ключа по длине слова
+    """
+    #Русские буквы в нижнем / верхнем регистре
     russian_letters = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
     russian_letters = russian_letters.upper()
     if length <= 0:
@@ -10,6 +13,9 @@ def generate_key_func(length):
     return key_word.upper()
 
 def binary_key_word_func(key_word):
+    """
+    перевод сгенерированного ключа в двоичное значение
+    """
     binary_key_word = ''
     for char in key_word:
         for byte in char.encode('utf-8'):
@@ -17,16 +23,37 @@ def binary_key_word_func(key_word):
     return binary_key_word
 
 def binary_original_word(original_word):
+    """
+    перевод введенного слова в двоичное значение
+    """
     binary_original_word = ''
     for char in original_word:
         for byte in char.encode('utf-8'):
             binary_original_word += f'{byte:08b}'
     return binary_original_word
 
+def length(binary_original_word, binary_key_word):
+    """
+    выравнивание двоичных значений слова и ключа по длине
+    """
+    max_length = max(len(binary_original_word), len(binary_key_word))
+    binary_original_word = binary_original_word.zfill(max_length)
+    binary_key_word = binary_key_word.zfill(max_length)
+    return (binary_original_word, binary_key_word)
+
+def encrypt_func(b_o_w, b_k_w, m_l):
+    """
+    операция XOR (побитового сложения значений слова и ключа)
+    """
+    encrypted_result = ""
+    for byte in range(m_l):
+        if b_o_w[byte] != b_k_w[byte]:
+            encrypted_result += "1"
+        else:
+            encrypted_result += "0"
+    return encrypted_result
 
 
-def encrypt_func(binary_original_word, binary_key_word):
-    ...
 
 
 original_word = input().upper()
